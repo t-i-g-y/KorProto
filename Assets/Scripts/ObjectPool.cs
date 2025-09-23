b1ad73b1571d96fc2c02bd13fb77067a4ec7e2cc
t-i-g-y <153322300+t-i-g-y@users.noreply.github.com>
2025-09-23 12:20:41 +0300

    fix: sync

--- Assets/Scripts/ObjectPool.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ObjectPool : MonoBehaviour
-{
-    [Header("Trains")]
-    public static GameObject TrainPrefab;
-    public static List<GameObject> TrainPool = new();
-}
