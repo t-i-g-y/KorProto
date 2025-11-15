242c3a53e144bbd8d578fbc03493721f15455fd9
t-i-g-y <153322300+t-i-g-y@users.noreply.github.com>
2025-11-15 16:29:47 +0300

    fix: sync update (unfinished feature)

---
+++ Assets/Scripts/RailBuilder/UI/UIRailListController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIRailListController : MonoBehaviour
+{
+    [SerializeField] private Button viewListButton;
+    [SerializeField] private GameObject listPanel;
+    [SerializeField] private GameObject listEntryPrefab;
+
+    void Awake()
+    {
+        listPanel.SetActive(false);
+    } 
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
